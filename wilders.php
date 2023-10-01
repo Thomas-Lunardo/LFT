31927709c703ab07b00343bbab6b73b137565379
nicky <nickyrasa@gmail.com>
2023-10-01 16:38:54 +0200

    add desktop photos v2 and php

---
+++ wilders.php
@@ -0,0 +1,20 @@
+<?php
+
+$teamwilders = [
+    ['wilders' => 'amina', 'lien' => './wildersamina.html', 'image' => './assets/images/wilders/aminav2.png',],
+    ['wilders' => 'benjamin', 'lien' => './wildersbenjamin.html', 'image' => './assets/images/wilders/benjaminv2.png',],
+    ['wilders' => 'victor', 'lien' => './wildersvictor.html', 'image' => './assets/images/wilders/victorv2.png',],
+    ['wilders' => 'thomas', 'lien' => './wildersthomas.html', 'image' => './assets/images/wilders/thomasv2.png',], 
+    ['wilders' => 'paul', 'lien' => './wilderspaul.html', 'image' => './assets/images/wilders/paulv2.png',],
+    ['wilders' => 'merwan', 'lien' => './wildersmerwan.html', 'image' => './assets/images/wilders/merwanv2.png',],
+    ['wilders' => 'nicky', 'lien' => './wildersnicky.html', 'image' => './assets/images/wilders/nickyv2.png',],
+    ['wilders' => 'plouis', 'lien' => './wildersplouis.html', 'image' => './assets/images/wilders/plouisv2.png',],
+    ['wilders' => 'aude', 'lien' => './wildersaude.html', 'image' => './assets/images/wilders/audev2.png',],
+    ['wilders' => 'ryad', 'lien' => './wildersryad.html', 'image' => './assets/images/wilders/ryadv2.png',],
+    ['wilders' => 'romain', 'lien' => './wildersromain.html', 'image' => './assets/images/wilders/romainv2.png',],
+];
+
+$amina = './assets/images/wilders/aminav2.png';
+readfile($amina);
+
+?>
