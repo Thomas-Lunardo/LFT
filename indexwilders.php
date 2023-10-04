b9ffbaeaf092773d2e49b4253bc4ccab758e6397
NICKY RASAMOELINA <nickyrasa@gmail.com>
2023-10-04 17:24:03 +0200

    menuburger premiere partie ajoutée

--- indexwilders.php
+++ indexwilders.php
@@ -54,6 +54,35 @@ function showwilders()
 
 
 <body>
+    <nav>
+        <div class="navbar">
+            <div class="container nav-container">
+                <input class="checkbox" type="checkbox" name="" id="" />
+                <div class="hamburger-lines">
+                    <span class="line line1"></span>
+                    <span class="line line2"></span>
+                    <span class="line line3"></span>
+                </div>
+                <div class="menu-items">
+                    <li><a href="#">Home</a></li>
+                    <ul><a href="#">Wilders</a>
+                        <li class="sousmenu"><ahref="#">Amina</a></li>
+                        <li class="sousmenu"><ahref="#">Benjamin</a></li>
+                        <li class="sousmenu"><ahref="#">Victor</a></li>
+                        <li class="sousmenu"><ahref="#">Thomas</a></li>
+                        <li class="sousmenu"><ahref="#">Paul</a></li>
+                        <li class="sousmenu" ><ahref="#">Merwan</a></li>
+                        <li><ahref="#">Nicky</a></li>
+                        <li><ahref="#">Pierre-Louis</a></li>
+                        <li><ahref="#">Aude</a></li>
+                        <li><ahref="#">Ryad</a></li>
+                        <li><ahref="#">Romain</a></li>
+                    </ul>
+                    <li><a href="#">A propos</a></li>
+                </div>
+            </div>
+        </div>
+    </nav>
     <main>
         <div class='containers'>
             <?php echo showwilders() ?>
