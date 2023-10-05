4f9b45495b5a3cd38bb6767a3cd1a49bdec2022f
NICKY RASAMOELINA <nickyrasa@gmail.com>
2023-10-05 14:32:32 +0200

    changed indexwilders

--- indexwilders.php
+++ indexwilders.php
@@ -1,47 +1,5 @@
 <?php
-<<<<<<< HEAD
-
-function showWilders()
-{
-    $images = array(
-        './images/wilders/aminav2.png',
-        './images/wilders/victorv2.png',
-        './images/wilders/thomasv2.png',
-        './images/wilders/merwanv2.png',
-        './images/wilders/benjaminv2.png',
-        './images/wilders/paulv2.png',
-        './images/wilders/nickyv2.png',
-        './images/wilders/audev2.png',
-        './images/wilders/wilders.png',
-        './images/wilders/ryadv2.png',
-        './images/wilders/pierrelouisv2.png',
-        './images/wilders/romainv2.png',
-
-    );
-
-    $links = array(
-        './wildersamina.html',
-        './wildersvictor.html',
-        './wildersthomas.html',
-        './wildersmerwan.html',
-        './wildersbenjamin.html',
-        './wilderspaul.html',
-        './wildersnicky.html',
-        './wildersaude.html',
-        './aboutus.html',
-        './wildersryad.html',
-        './wildersplouis.html',
-        './wildersromain.html',
-
-    );
-
-    for ($i = 0; $i < count($images); $i++) {
-        echo "<a href =  '$links[$i]' ><img src= '$images[$i]'/></a> ";
-    };
-};
-=======
 include 'function.php'
->>>>>>> 71f1e791fafe7f3a620c70a35902e1a6846219d2
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -57,7 +15,7 @@ include 'function.php'
     <nav class="navbar-mobile">
    <i class="fa-solid fa-bars"></i>
         <div class="nav-list-mobile">
-           <li> <a href="/index.html">Home</a></li>
+           <li> <a href="/index.php">Home</a></li>
               <ul> <a href="/indexWilders.php">Wilders</a>
                <li> <a href="/wildersAmina.html">Amina</a></li>
                <li> <a href="/wildersBenjamin.html">Benjamin</a></li>
@@ -74,13 +32,8 @@ include 'function.php'
         </div>
     </nav>
     <main>
-<<<<<<< HEAD
-        <div class='containers'>
-            <?php echo showWilders() ?>
-=======
         <div class="containers">
         <?php showWilders() ?> 
->>>>>>> 71f1e791fafe7f3a620c70a35902e1a6846219d2
         </div>
     </main>
              <script src="index.js">
